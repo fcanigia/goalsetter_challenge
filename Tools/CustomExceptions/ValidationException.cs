a61fbf67d896f7bef34eeb5b1956720fd3594dca
Facundo Canigia <facundo.canigia@gmail.com>
2024-04-06 16:52:40 -0300

    Create custom exceptions

---
+++ Tools/CustomExceptions/ValidationException.cs
@@ -0,0 +1,18 @@
+﻿namespace GoalsetterChallenge.Tools.CustomExceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException()
+    {
+    }
+
+    public ValidationException(string message)
+        : base(message)
+    {
+    }
+
+    public ValidationException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
