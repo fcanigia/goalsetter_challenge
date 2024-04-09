ffae4b0e5dd3fda5b4b9ed1d408682cefa41c894
Facundo Canigia <facundo.canigia@gmail.com>
2024-04-09 00:01:41 -0300

    Some more tests

--- tests/Rental.UnitTests/RentalService_Should.cs
+++ tests/Rental.UnitTests/RentalService_Should.cs
@@ -18,6 +18,8 @@ public class RentalService_Should
     private readonly Vehicle _vehicle1 = new() { Id = 1, DailyPrice = 10.01, Model = "Golf", Brand = "Volkswagen", Year = 2023, Type = "Car" };
     private readonly Client _client1 = new() { Id = 1, FirstName = "John", LastName = "Doe", EmailAddress = "JohnDoe@gmail.com", PhoneNumber = "+54 9 11 3232 1212" };
 
+    private readonly Vehicle _vehicleRemoved = new() { Id = 1, DailyPrice = 10.01, Model = "Golf", Brand = "Volkswagen", Year = 2023, Type = "Car", IsRemoved = true };
+    private readonly Client _clientRemoved = new() { Id = 1, FirstName = "John", LastName = "Doe", EmailAddress = "JohnDoe@gmail.com", PhoneNumber = "+54 9 11 3232 1212", IsRemoved = true };
 
     public RentalService_Should()
     {
@@ -37,6 +39,24 @@ public class RentalService_Should
 
         _clientServiceMock.Setup(service => service.GetById(rentalDto.ClientId))
                           .ReturnsAsync(_client1);
+    }
+
+    private void SetupRemovedVehicleAndClientMocks(RentalInDto rentalDto)
+    {
+        _vehicleServiceMock.Setup(service => service.GetById(rentalDto.VehicleId))
+                           .ReturnsAsync(_vehicleRemoved);
+
+        _clientServiceMock.Setup(service => service.GetById(rentalDto.ClientId))
+                          .ReturnsAsync(_client1);
+    }
+
+    private void SetupVehicleAndRemovedClientMocks(RentalInDto rentalDto)
+    {
+        _vehicleServiceMock.Setup(service => service.GetById(rentalDto.VehicleId))
+                           .ReturnsAsync(_vehicle1);
+
+        _clientServiceMock.Setup(service => service.GetById(rentalDto.ClientId))
+                          .ReturnsAsync(_clientRemoved);
     }
 
     [Fact]
@@ -137,5 +157,53 @@ public class RentalService_Should
         // Assert
         await action.Should().ThrowAsync<ValidationException>()
             .WithMessage("EndDate cannot be before StartDate");
+    }
+
+    [Fact]
+    public async void CreateRental_ReturnsError_WhenVehicleRemoved()
+    {
+        // Arrange
+        var rentalDto = new RentalInDto
+        {
+            VehicleId = 1,
+            ClientId = 1,
+            StartDate = DateTime.UtcNow.AddDays(1),
+            EndDate = DateTime.UtcNow.AddDays(3)
+        };
+
+        SetupRemovedVehicleAndClientMocks(rentalDto);
+
+        var rentalService = new RentalService(_context, _clientServiceMock.Object, _vehicleServiceMock.Object);
+
+        // Act
+        Func<Task> action = async () => await rentalService.Create(rentalDto);
+
+        // Assert
+        await action.Should().ThrowAsync<ValidationException>()
+            .WithMessage("Vehicle with Id 1 was removed from the Rental and cannot be selected");
+    }
+
+    [Fact]
+    public async void CreateRental_ReturnsError_WhenClientRemoved()
+    {
+        // Arrange
+        var rentalDto = new RentalInDto
+        {
+            VehicleId = 1,
+            ClientId = 1,
+            StartDate = DateTime.UtcNow.AddDays(1),
+            EndDate = DateTime.UtcNow.AddDays(3)
+        };
+
+        SetupVehicleAndRemovedClientMocks(rentalDto);
+
+        var rentalService = new RentalService(_context, _clientServiceMock.Object, _vehicleServiceMock.Object);
+
+        // Act
+        Func<Task> action = async () => await rentalService.Create(rentalDto);
+
+        // Assert
+        await action.Should().ThrowAsync<ValidationException>()
+            .WithMessage("Client with Id 1 was removed from the Rental and cannot rent new cars");
     }
 }
